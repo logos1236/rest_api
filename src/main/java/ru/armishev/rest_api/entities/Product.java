b726f06fb2163370924430f0ce2dbae2432472dc
logos <armishev@vigroup.ru>
2021-01-03 17:47:05 +0500

    Price

--- src/main/java/ru/armishev/rest_api/entities/Product.java
+++ src/main/java/ru/armishev/rest_api/entities/Product.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name="product")
+@SecondaryTable(name = "price_1", pkJoinColumns=@PrimaryKeyJoinColumn(name="product_id", referencedColumnName="id"))
 public class Product {
     @Id
     @Column(name="id", nullable = false)
@@ -12,6 +13,17 @@ public class Product {
 
     @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(table= "price_1", name="val")
+    private Long price;
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
 
     public long getId() {
         return id;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,11 @@ INSERT INTO product (id, name) VALUES (1, 'Product 1');
 INSERT INTO product (id, name) VALUES (2, 'Product 2');
 INSERT INTO product (id, name) VALUES (3, 'Product 3');
 INSERT INTO product (id, name) VALUES (4, 'Product 4');
+INSERT INTO product (id, name) VALUES (5, 'Product 5');
+INSERT INTO price_1 (product_id, val) VALUES (1, 100);
+INSERT INTO price_1 (product_id, val) VALUES (2, 400);
+INSERT INTO price_1 (product_id, val) VALUES (3, 300);
+INSERT INTO price_1 (product_id, val) VALUES (4, 500);
 
 <!--INSERT INTO sales (id, price, date_good_incoming, date_sale, product) VALUES (1, 50000, '2018-7-04', '2019-3-04', 1);-->
 <!--INSERT INTO sales (id, price, date_good_incoming, date_sale, product) VALUES (2, 60000, '2018-7-05', '2019-4-04', 2);-->
